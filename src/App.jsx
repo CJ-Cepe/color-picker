dd1684c8b6eff799f3432cce7b27b04275e462f2
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-03-02 23:23:14 +0800

    Feat: implement eye dropper and update div and fields' color & equivalent

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useRef, useState } from 'react';
+import convertHex from './convertColor';
 import ColorWell from './ColorWell'
 import Field from './Field'
 
 
+
 function App() {
+
+  const [colors, setColors] = useState({})
+  const currentColorRef = useRef(null)
+
+  const handleColorPick = async () => {
+    const eyeDropper =  new EyeDropper();
+    try {
+      const selectedColor = await eyeDropper.open();
+      updateColor(selectedColor.sRGBHex)
+    } catch (error) {
+        console.error('Error selecting color:', error);
+    }
+  }
+
+  function updateColor(hex){
+    currentColorRef.current.style.backgroundColor = hex;
+    const extractedColors = convertHex(hex)
+    setColors(extractedColors)
+  }
 
   return (
     <>
@@ -10,12 +32,12 @@ function App() {
       <h1>Color Picker</h1>
     </header>
     <main>
-      <div></div>
+      <div ref={currentColorRef}></div>
       <div>
-        <Field labelName = "HEX:" value = ""></Field>
-        <Field labelName = "HSL:" value = ""></Field>
-        <Field labelName = "RGB:" value = ""></Field>
-        <Field labelName = "RGBA:" value = ""></Field>
+        <Field labelName = "HEX:" value = {colors.hex}></Field>
+        <Field labelName = "HSL:" value = {colors.hsl}></Field>
+        <Field labelName = "RGB:" value = {colors.rgb}></Field>
+        <Field labelName = "RGBA:" value = {colors.rgba}></Field>
       </div>
     </main>
     <aside>
@@ -23,31 +45,9 @@ function App() {
       <ColorWell></ColorWell>
       <ColorWell></ColorWell>
       <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
-      <ColorWell></ColorWell>
     </aside>
     <section>
-      <input type="color" />
+      <button onClick={handleColorPick}>Pick Color</button>
       <button>download</button>
       <button>delete</button>
       <button>clear</button>
