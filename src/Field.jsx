2afd581478a95e1cbf33daa1e5b8036d8b7605c0
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-03-03 21:56:11 +0800

    Feat(input): copy input value to clipboard when clicked

--- src/Field.jsx
+++ src/Field.jsx
@@ -1,8 +1,18 @@
+async function copyContent(e){
+    try {
+        if(e.target.value !== "---------"){
+            await navigator.clipboard.writeText(e.target.value);
+        }
+    } catch(err){
+
+    }
+}
+
 function Field({labelName, value=""}){
     return (
     <label htmlFor="">
         {labelName}
-        <input type="text" value={value} readOnly/>
+        <input type="text" value={value} readOnly onClick={copyContent}/>
     </label>
     )
 }
