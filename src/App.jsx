820bb4d4c85ba569f4431cd5df8506296c70b504
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-03-09 23:26:58 +0800

    Feat: toggle root and dots, also add delay to color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import { useRef, useState, useEffect } from 'react';
 import convertHex from './convertColor';
 import handleDownload from './handleDownload'
@@ -9,7 +10,6 @@ import clearIcon from './assets/clear.svg'
 
 
 function App() {
-
   let defColorList = []
   const defColor = {
     hex: '--------------',
@@ -20,6 +20,8 @@ function App() {
 
   const [colorList, setColorList] = useState(defColorList) //hex list
   const [colors, setColors] = useState(defColor) //obj of colors
+  const rootRef = useRef(null)
+  const dotsRef = useRef(null)
   const currentColorRef = useRef(null)
 
   useEffect(() => {
@@ -29,17 +31,35 @@ function App() {
         setColorList(defColorList)
       }
     })
+
+    rootRef.current = document.querySelector("#root");
+    dotsRef.current = document.querySelector('.snippet')
   }, [])
 
+  function toggleHide(){
+    rootRef.current.classList.toggle('invisible')
+    dotsRef.current.classList.toggle('invisible')
+  }
+
+  function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
   const handleColorPick = async () => {
+    toggleHide()
     const eyeDropper =  new EyeDropper();
     try {
-      const selectedColor = await eyeDropper.open();
+      await delay(10);
+      const selectedColor = await eyeDropper.open();  
       updateColor(selectedColor.sRGBHex)
     } catch (error) {
        /*  console.error('Error selecting color:', error); */
+    } finally {
+      toggleHide()
     }
   }
+
+  //eye dropper escaped, right clicked, error?, clicked on screen
 
   function sendMessage(updatedColorList){
     /* console.log({updatedColorList}) */
@@ -87,34 +107,34 @@ function App() {
  
   return (
     <>
-    <header>
-      <img src='icon.png' alt="HueMore, a simple Color Picker title icon"/>
-      <h1>HueMore<span> Color Picker</span></h1>
-      <a title="Open user guide" href='https://color-picker-7v4.pages.dev/' target='_blank'>User Guide</a>
-    </header>
-    <main>
-      <div ref={currentColorRef} style={{backgroundColor: colors.hex}}>?</div>
-      <div>
-        <Field labelName = "HEX:" value = {colors.hex}></Field>
-        <Field labelName = "HSL:" value = {colors.hsl}></Field>
-        <Field labelName = "RGB:" value = {colors.rgb}></Field>
-        <Field labelName = "CMYK:" value = {colors.cmyk}></Field>
-      </div>
-    </main>
-    <aside>
-      <span>
-      <ColorWell>+</ColorWell>
-        {
-          colorList.map((color, index) => <ColorWell key={index} color={color} onClick={() => updateColor(color, false)}></ColorWell>)
-        }
-      </span>
-    </aside>
-    <section>
-      <button onClick={handleColorPick} title="Pick a color">Pick Color</button>
-      <button onClick={() => handleDownload(colorList)} title="Export color list"><img src={downloadIcon} alt="Download"/></button>
-      <button onClick={removeColorToList} title="Delete a color"><img src={deleteIcon} alt="Delete"/></button>
-      <button onClick={handleClearList} title="Clear colors list"><img src={clearIcon} alt="Clear"/></button>
-    </section>
+      <header>
+        <img src='icon.png' alt="HueMore, a simple Color Picker title icon"/>
+        <h1>HueMore<span> Color Picker</span></h1>
+        <a title="Open user guide" href='https://color-picker-7v4.pages.dev/' target='_blank'>User Guide</a>
+      </header>
+      <main>
+        <div ref={currentColorRef} style={{backgroundColor: colors.hex}}>?</div>
+        <div>
+          <Field labelName = "HEX:" id="hex" value = {colors.hex}></Field>
+          <Field labelName = "HSL:" id="hsl" value = {colors.hsl}></Field>
+          <Field labelName = "RGB:" id="rgb" value = {colors.rgb}></Field>
+          <Field labelName = "CMYK:" id="cmyk" value = {colors.cmyk}></Field>
+        </div>
+      </main>
+      <aside>
+        <span>
+        <ColorWell>+</ColorWell>
+          {
+            colorList.map((color, index) => <ColorWell key={index} color={color} onClick={() => updateColor(color, false)}></ColorWell>)
+          }
+        </span>
+      </aside>
+      <section>
+        <button onClick={handleColorPick} title="Pick a color">Pick Color</button>
+        <button onClick={() => handleDownload(colorList)} title="Export color list"><img src={downloadIcon} alt="Download"/></button>
+        <button onClick={removeColorToList} title="Delete a color"><img src={deleteIcon} alt="Delete"/></button>
+        <button onClick={handleClearList} title="Clear colors list"><img src={clearIcon} alt="Clear"/></button>
+      </section>
     </>
   )
 }
