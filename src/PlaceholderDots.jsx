2c3d687de0c4de6a9773cf065b3d821fcf233c27
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-03-09 23:26:16 +0800

    Feat: add placeholder when color picker is active

---
+++ src/PlaceholderDots.jsx
@@ -0,0 +1,11 @@
+function PlaceholderDots () {
+    return (
+        <>
+            <div class="stage">
+                <div class="dot-flashing"></div>
+            </div>
+        </>
+    )
+}
+
+export default PlaceholderDots
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import PlaceholderDots from './PlaceholderDots.jsx'
 import App from './App.jsx'
 import './normalize.css'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const tempDiv = document.createElement('div')
+document.body.appendChild(tempDiv)
+tempDiv.classList.add('invisible', 'snippet')
+tempDiv.setAttribute('data-title', 'dot-flashing')
+
+ReactDOM.createRoot(tempDiv).render(
+  <React.StrictMode>
+    <PlaceholderDots />
+  </React.StrictMode>,
+)
+
